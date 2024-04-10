34832c7147dac68fe3c6f918ee49fb845f3e5f84
aphelps94 <aphelps94@charter.net>
2024-04-09 20:06:59 -0400

    Added explanation
    Committed changes to fork

--- com/thejask/example/HelloWorld.java
+++ com/thejask/example/HelloWorld.java
@@ -4,5 +4,6 @@ public class HelloWorld{
     public static void main(String[] args) {
         System.out.println("Hi from Java world!");
 		System.out.println("My name is Aaron.");
+		System.out.println("I am committing these changes.");
     }
 }
